0717e77c237932c28e238fbce3603359349ac7a8
hye219 <jhy_0219@daum.net>
2022-09-14 12:43:56 +0900

    android Java_텍스트,이미지 등 sns공유하기

--- useInstagramAPI_Java/app/src/main/java/com/example/useinstagramapi_java/MainActivity.java
+++ useInstagramAPI_Java/app/src/main/java/com/example/useinstagramapi_java/MainActivity.java
@@ -15,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // activity_main의 버튼을 가져오기
+        Button shareButton = findViewById(R.id.shareButton);
+
+        // 클릭 리스너 구현
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ACTION_SEND를 이용해 공유하기 창 띄우기
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                //intent로 보낼 공유 내용의 타입을 정하기
+                intent.setType("text/plain");
+                intent.setType("image/gif");
+                intent.setType("image/jpeg");
+
+                //String으로 받아서 넣기
+                String sendMessage = "이렇게 스트링으로 만들어서 넣어주면 됩니다.";
+                //String sendImage = "ic_launcher.webp";
+                intent.putExtra(Intent.EXTRA_TEXT, sendMessage);
+                //intent.putExtra(Intent.image)
+
+                Intent shareIntent = Intent.createChooser(intent, "share");
+                startActivity(shareIntent);
+            }
+        });
     }
 }
