e44b3b742ea6b9e3d032883a2263bb803c6c1218
hye219 <jhy_0219@daum.net>
2022-09-14 12:37:14 +0900

    android Java_first commit

---
+++ useInstagramAPI_Java/app/src/main/java/com/example/useinstagramapi_java/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.useinstagramapi_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
