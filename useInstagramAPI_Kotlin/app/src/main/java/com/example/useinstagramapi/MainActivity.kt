73f2c093df0733ec5c971ab75d3065476c1ae718
hye219 <jhy_0219@daum.net>
2022-09-14 19:24:03 +0900

    data binding 사용하기

--- useInstagramAPI_Kotlin/app/src/main/java/com/example/useinstagramapi/MainActivity.kt
+++ useInstagramAPI_Kotlin/app/src/main/java/com/example/useinstagramapi/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.useinstagramapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.useinstagramapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //DataBindingUtil class의 객체를 생성
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //기존의 setContentView()를 DataBindingUtil.setContentView()로 대체한다.
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
     }
 }
